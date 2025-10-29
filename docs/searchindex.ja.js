4512c5460980bbb131de821b1c35a5b4a8d790a2
Hironobu HASEGAWA <hasegawa@akita-nct.ac.jp>
2025-10-29 11:44:49 +0900

    Add english contents

--- docs/searchindex.ja.js
+++ docs/searchindex.ja.js
@@ -201,86 +201,6 @@ var relearn_searchindex = [
     "content": "",
     "description": "",
     "tags": [],
-    "title": "タグ :: Calendar",
-    "uri": "/tags/calendar/index.html"
-  },
-  {
-    "breadcrumb": "Home \u003e タグ",
-    "content": "",
-    "description": "",
-    "tags": [],
-    "title": "タグ :: Google",
-    "uri": "/tags/google/index.html"
-  },
-  {
-    "breadcrumb": "Home \u003e タグ",
-    "content": "",
-    "description": "",
-    "tags": [],
-    "title": "タグ :: Ical",
-    "uri": "/tags/ical/index.html"
-  },
-  {
-    "breadcrumb": "Home \u003e タグ",
-    "content": "",
-    "description": "",
-    "tags": [],
-    "title": "タグ :: R",
-    "uri": "/tags/r/index.html"
-  },
-  {
-    "breadcrumb": "Home \u003e メモ \u003e R",
-    "content": "背景 Googleカレンダーの整理をしていたら，カレンダー内の予定がUTCとなっていた． カレンダー表示のときには通常通りJSTの時間で表示されているが，予定の詳細を見たり編集する際にUTCとなっている．\n手作業で直す場合，タイムゾーンを変えても時間を表す数字は変わらないので，+9:00する必要がある． そもそも件数が多いのでやっていられない．\n色々と探った結果，ical形式でエクスポートすると，カレンダーの設定によらずUTCで出力されることが判明した．フィードバックは送ったが，待っていられないし，そもそも仕様の可能性もある．\nそのため，以下のようなRスクリプトを書いて，icalを修正することにした．\n成果物 if (!require(\"pacman\")) install.packages(\"pacman\"); library(pacman) p_load(tidyverse) setwd(\"icalファイルの場所\") # ical（拡張子はics）ファイルを文字列ベクトルで読み込み data \u003c- read_lines(\"Calendar.ics\") # 修正対象はDTSTART,DTEND,DTSTAMP,CREATED,LAST-MODIFIED ## lubridateパッケージのsecond minute hour day month yearを使って最初に\"TZID=Asia/Tokyo:\"以下の部分を作っておいて，その後にmutateする方が良いと思うが，直す気力がない． dat \u003c- tibble(raw=data, jst=with_tz(ymd_hms(str_sub(data,9)), \"Asia/Tokyo\")) %\u003e% mutate(DTSTART= paste0(\"DTSTART;TZID=Asia/Tokyo:\",str_sub(jst,1,4),str_sub(jst,6,7),str_sub(jst,9,10),\"T\",str_sub(jst,12,13),str_sub(jst,15,16),str_sub(jst,18,19)), DTEND= paste0(\"DTEND;TZID=Asia/Tokyo:\",str_sub(jst,1,4),str_sub(jst,6,7),str_sub(jst,9,10),\"T\",str_sub(jst,12,13),str_sub(jst,15,16),str_sub(jst,18,19)), DTSTAMP= paste0(\"DTSTAMP;TZID=Asia/Tokyo:\",str_sub(jst,1,4),str_sub(jst,6,7),str_sub(jst,9,10),\"T\",str_sub(jst,12,13),str_sub(jst,15,16),str_sub(jst,18,19)), CREATED = paste0(\"CREATED;TZID=Asia/Tokyo:\",str_sub(jst,1,4),str_sub(jst,6,7),str_sub(jst,9,10),\"T\",str_sub(jst,12,13),str_sub(jst,15,16),str_sub(jst,18,19)), \"LAST-MODIFIED\"= paste0(\"LAST-MODIFIED;TZID=Asia/Tokyo:\",str_sub(jst,1,4),str_sub(jst,6,7),str_sub(jst,9,10),\"T\",str_sub(jst,12,13),str_sub(jst,15,16),str_sub(jst,18,19))) out \u003c- ifelse(str_detect(dat$raw, pattern=\"DTSTART:2\"),dat$DTSTART, # カレンダーの設定情報である\"DTSTART:19700101T000000\"を除外するため\"DTSTART:2\"とする ifelse(str_detect(dat$raw, pattern=\"DTEND:\"),dat$DTEND, ifelse(str_detect(dat$raw, pattern=\"DTSTAMP:\"),dat$DTSTAMP, ifelse(str_detect(dat$raw, pattern=\"CREATED:\"),dat$CREATED, ifelse(str_detect(dat$raw, pattern=\"LAST-MODIFIED:\"),dat$\"LAST-MODIFIED\", dat$raw))))) # ファイルに書き出し write_lines(out, \"FixedCalendar.ics\", na = \"NA\", append = FALSE) 同じ事象に悩む方のお役に立てば幸いです．",
-    "description": "背景 Googleカレンダーの整理をしていたら，カレンダー内の予定がUTCとなっていた． カレンダー表示のときには通常通りJSTの時間で表示されているが，予定の詳細を見たり編集する際にUTCとなっている．\n手作業で直す場合，タイムゾーンを変えても時間を表す数字は変わらないので，+9:00する必要がある． そもそも件数が多いのでやっていられない．\n色々と探った結果，ical形式でエクスポートすると，カレンダーの設定によらずUTCで出力されることが判明した．フィードバックは送ったが，待っていられないし，そもそも仕様の可能性もある．\nそのため，以下のようなRスクリプトを書いて，icalを修正することにした．\n成果物 if (!require(\"pacman\")) install.packages(\"pacman\"); library(pacman) p_load(tidyverse) setwd(\"icalファイルの場所\") # ical（拡張子はics）ファイルを文字列ベクトルで読み込み data \u003c- read_lines(\"Calendar.ics\") # 修正対象はDTSTART,DTEND,DTSTAMP,CREATED,LAST-MODIFIED ## lubridateパッケージのsecond minute hour day month yearを使って最初に\"TZID=Asia/Tokyo:\"以下の部分を作っておいて，その後にmutateする方が良いと思うが，直す気力がない． dat \u003c- tibble(raw=data, jst=with_tz(ymd_hms(str_sub(data,9)), \"Asia/Tokyo\")) %\u003e% mutate(DTSTART= paste0(\"DTSTART;TZID=Asia/Tokyo:\",str_sub(jst,1,4),str_sub(jst,6,7),str_sub(jst,9,10),\"T\",str_sub(jst,12,13),str_sub(jst,15,16),str_sub(jst,18,19)), DTEND= paste0(\"DTEND;TZID=Asia/Tokyo:\",str_sub(jst,1,4),str_sub(jst,6,7),str_sub(jst,9,10),\"T\",str_sub(jst,12,13),str_sub(jst,15,16),str_sub(jst,18,19)), DTSTAMP= paste0(\"DTSTAMP;TZID=Asia/Tokyo:\",str_sub(jst,1,4),str_sub(jst,6,7),str_sub(jst,9,10),\"T\",str_sub(jst,12,13),str_sub(jst,15,16),str_sub(jst,18,19)), CREATED = paste0(\"CREATED;TZID=Asia/Tokyo:\",str_sub(jst,1,4),str_sub(jst,6,7),str_sub(jst,9,10),\"T\",str_sub(jst,12,13),str_sub(jst,15,16),str_sub(jst,18,19)), \"LAST-MODIFIED\"= paste0(\"LAST-MODIFIED;TZID=Asia/Tokyo:\",str_sub(jst,1,4),str_sub(jst,6,7),str_sub(jst,9,10),\"T\",str_sub(jst,12,13),str_sub(jst,15,16),str_sub(jst,18,19))) out \u003c- ifelse(str_detect(dat$raw, pattern=\"DTSTART:2\"),dat$DTSTART, # カレンダーの設定情報である\"DTSTART:19700101T000000\"を除外するため\"DTSTART:2\"とする ifelse(str_detect(dat$raw, pattern=\"DTEND:\"),dat$DTEND, ifelse(str_detect(dat$raw, pattern=\"DTSTAMP:\"),dat$DTSTAMP, ifelse(str_detect(dat$raw, pattern=\"CREATED:\"),dat$CREATED, ifelse(str_detect(dat$raw, pattern=\"LAST-MODIFIED:\"),dat$\"LAST-MODIFIED\", dat$raw))))) # ファイルに書き出し write_lines(out, \"FixedCalendar.ics\", na = \"NA\", append = FALSE) 同じ事象に悩む方のお役に立てば幸いです．",
-    "tags": [
-      "R",
-      "Ical",
-      "Google",
-      "Calendar",
-      "カレンダー"
-    ],
-    "title": "Rを使ってicalファイルのタイムゾーンを修正する",
-    "uri": "/memo/r/fix_ical_using_r/index.html"
-  },
-  {
-    "breadcrumb": "Home",
-    "content": "",
-    "description": "",
-    "tags": [],
-    "title": "タグ",
-    "uri": "/tags/index.html"
-  },
-  {
-    "breadcrumb": "Home \u003e タグ",
-    "content": "",
-    "description": "",
-    "tags": [],
-    "title": "タグ :: カレンダー",
-    "uri": "/tags/%E3%82%AB%E3%83%AC%E3%83%B3%E3%83%80%E3%83%BC/index.html"
-  },
-  {
-    "breadcrumb": "Home \u003e メモ",
-    "content": "使用テーマの解説 https://learn.netlify.app/en/\nx. Markdown CheetSheet Text Format Italic（斜体） Italic（斜体）\nEmphasis（強調） Emphasis（強調）\nStrikethrough（取り消し線）\nThis is inline.\nList text test test text test test text test test Horizontal rules Blockquotes（引用） This is Blockquotes\nLinks（参照） yonehub blog\nImages（画像） Tables（表） id name date 1 test 2019-01-01 2 test 2019-01-02 3 test 2019-01-03",
-    "description": "使用テーマの解説 https://learn.netlify.app/en/\nx. Markdown CheetSheet Text Format Italic（斜体） Italic（斜体）\nEmphasis（強調） Emphasis（強調）\nStrikethrough（取り消し線）\nThis is inline.\nList text test test text test test text test test Horizontal rules Blockquotes（引用） This is Blockquotes\nLinks（参照） yonehub blog\nImages（画像） Tables（表） id name date 1 test 2019-01-01 2 test 2019-01-02 3 test 2019-01-03",
-    "tags": [
-      "Hugo"
-    ],
-    "title": "Hugo",
-    "uri": "/memo/hugo/index.html"
-  },
-  {
-    "breadcrumb": "Home \u003e タグ",
-    "content": "",
-    "description": "",
-    "tags": [],
-    "title": "タグ :: Hugo",
-    "uri": "/tags/hugo/index.html"
-  },
-  {
-    "breadcrumb": "Home \u003e タグ",
-    "content": "",
-    "description": "",
-    "tags": [],
     "title": "タグ :: Dokuwiki",
     "uri": "/tags/dokuwiki/index.html"
   },
@@ -297,8 +217,24 @@ var relearn_searchindex = [
     "content": "",
     "description": "",
     "tags": [],
+    "title": "タグ :: Hugo",
+    "uri": "/tags/hugo/index.html"
+  },
+  {
+    "breadcrumb": "Home \u003e タグ",
+    "content": "",
+    "description": "",
+    "tags": [],
     "title": "タグ :: Pandoc",
     "uri": "/tags/pandoc/index.html"
+  },
+  {
+    "breadcrumb": "Home",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "タグ",
+    "uri": "/tags/index.html"
   },
   {
     "breadcrumb": "Home \u003e タグ",
@@ -618,6 +554,14 @@ var relearn_searchindex = [
     "tags": [],
     "title": "QGIS",
     "uri": "/memo/qgis/index.html"
+  },
+  {
+    "breadcrumb": "Home \u003e タグ",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "タグ :: R",
+    "uri": "/tags/r/index.html"
   },
   {
     "breadcrumb": "Home \u003e メモ \u003e R",
